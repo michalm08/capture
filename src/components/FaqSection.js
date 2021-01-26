ce8bef98856b1d3fcc3a7afe1a7af6617550370d
michalm08 <michalmski@gmail.com>
2021-01-26 22:18:55 +0100

    17add toggle component to faqSection and animate

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -2,60 +2,58 @@ import React from 'react';
 import styled from 'styled-components';
 import { About } from '../styles';
 import Toggle from './Toggle';
+//detected if layout changes
+import { AnimateSharedLayout } from 'framer-motion';
+
 const FaqSection = () => {
   return (
     <Faq>
       <h2>
         Any Questions <span>FAQ</span>
       </h2>
-      <Toggle>
-        <div className='question'>
-          <h4>How Do I Start?</h4>
+      <AnimateSharedLayout>
+        <Toggle title='How Do I Start?'>
           <div className='answer'>
             <p>Lorem ipsum dolor sit amet.</p>
-
             <p>
               Lorem ipsum, dolor sit amet consectetur adipisicing elit.
               Provident, voluptate.
             </p>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum ad at alias rerum corrupti! Voluptatibus quia sint dolorem veniam quasi.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum ad
+              at alias rerum corrupti! Voluptatibus quia sint dolorem veniam
+              quasi.
+            </p>
           </div>
-          <div className='faq-line'></div>
-        </div>
-      </Toggle>
-      <div className='question'>
-        <h4>Daily Schedule</h4>
-        <div className='answer'>
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident,
-            voluptate.
-          </p>
-        </div>
-        <div className='faq-line'></div>
-      </div>
-      <div className='question'>
-        <h4>Diferrent Payment Methods</h4>
-        <div className='answer'>
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident,
-            voluptate.
-          </p>
-        </div>
-        <div className='faq-line'></div>
-      </div>
-      <div className='question'>
-        <h4>What Products do you offer.</h4>
-        <div className='answer'>
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident,
-            voluptate.
-          </p>
-        </div>
-        <div className='faq-line'></div>
-      </div>
+        </Toggle>
+        <Toggle title='Daily Schedule'>
+          <div className='answer'>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Provident, voluptate.
+            </p>
+          </div>
+        </Toggle>
+        <Toggle title='Diferrent Payment Methods'>
+          <div className='answer'>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Provident, voluptate.
+            </p>
+          </div>
+        </Toggle>
+        <Toggle title='What Products do you offer.'>
+          <div className='answer'>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Provident, voluptate.
+            </p>
+          </div>
+        </Toggle>
+      </AnimateSharedLayout>
     </Faq>
   );
 };
