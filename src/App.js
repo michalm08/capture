f81efb76bf918105cd97d68607ce4ea2e0f3bd84
michalm08 <michalmski@gmail.com>
2021-01-26 13:53:50 +0100

    9. Initial React Router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
-import React from 'react'
-import GlobalStyle from './components/GlobalStyle';
-import Nav from './components/Nav';
+import React from 'react';
+
 //Global Style
+import GlobalStyle from './components/GlobalStyle';
 
 //Import pages
-import AboutUs from './pages/AboutUs'
+import AboutUs from './pages/AboutUs';
+import ContactUs from './pages/ContactUs';
+import OurWork from './pages/OurWork';
+import Nav from './components/Nav';
+
+//Router
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <GlobalStyle/>
-      <Nav/>
-      <AboutUs/>
+    <div className='App'>
+      <GlobalStyle />
+      <Nav />
+      <Switch>
+        <Route path='/' exact component={AboutUs} />
+
+        <Route path='/work'>
+          <OurWork />
+        </Route>
+
+        <Route path='/contact'>
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
