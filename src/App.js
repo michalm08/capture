2551b065e5ccf447dd3c338ea706a293287a9880
michalm08 <michalmski@gmail.com>
2021-01-26 10:28:00 +0100

    2. About Section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
+//Import pages
+import AboutUs from './pages/AboutUs'
 
 function App() {
   return (
     <div className="App">
-      <h1>Capture</h1>
+      <AboutUs/>
     </div>
   );
 }
